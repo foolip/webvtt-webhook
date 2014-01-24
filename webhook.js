bb2ea2387f0c665ebd6de8142298ca6021375d3b
Philip Jägenstedt <philip@foolip.org>
2014-01-24 04:17:40 +0000

    Create a GitHub webhook to sync Git with CVS on push

---
+++ webhook.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  if (req.url == '/webvtt-webhook') {
+    fs.writeFile(__dirname + '/jobs/' + Date.now(), '', function(err) {
+      if (err) {
+	console.log(err);
+	res.statusCode = 500;
+      }
+      res.end(http.STATUS_CODES[res.statusCode]);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end(http.STATUS_CODES[res.statusCode]);
+  }
+}).listen(8777);
---
+++ webhook.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -e
+
+cd "$(dirname $0)"
+
+setup() {
+    mkdir -p jobs
+    if [ ! -e webvtt ]; then
+	git clone https://github.com/w3c/webvtt
+    fi
+}
+
+update() {
+    cd webvtt
+    git fetch origin master
+    if [ "$(git rev-parse HEAD)" != "$(git rev-parse FETCH_HEAD)" ]; then
+	git reset --hard FETCH_HEAD
+	git submodule update --init -f
+	./publish.sh -f 2>&1 | mail -s "webvtt-webhook update" root
+    fi
+    cd ..
+}
+
+server() {
+    node webhook.js &
+    trap "kill $!" EXIT
+    echo "server running"
+}
+
+worker() {
+    while true; do
+	echo "worker waiting"
+	inotifywait -e create -qq jobs
+	rm jobs/*
+	echo "worker working"
+	update
+	echo "worker resting"
+	sleep 1
+    done
+}
+
+setup
+server
+worker
